fc5cd0a1ca0d4810ebb8826304439ef99f283554
satwikmvv <satwikmvv@gmail.com>
2019-07-24 20:39:51 -0500

    deploy test

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,7 +15,7 @@ const reducers = combineReducers({
     ui: uiReducer
 });
 
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware)))
 
 
 export default store;
